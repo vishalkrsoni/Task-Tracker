e09c60048bbd582b5df8f1c8824dceb953263b77
vishalkrsoni <vishal.sony1@gmail.com>
2023-01-11 15:17:06 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Header from './MyComponents/Header';
-import { Todos } from './MyComponents/Todos';
-import { Footer } from './MyComponents/Footer';
-import { AddTodo } from './MyComponents/AddTodo';
-import { About } from './MyComponents/About';
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/jsx-no-undef */
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from "react";
 
 // import {
 //   BrowserRouter as Router,
@@ -15,19 +16,21 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   let initTodo;
-  if (localStorage.getItem('todos') === null) initTodo = [];
-  else initTodo = JSON.parse(localStorage.getItem('todos'))
+  if (localStorage.getItem("todos") === null) initTodo = [];
+  else initTodo = JSON.parse(localStorage.getItem("todos"));
 
   // deleting this way doesn't work
   // let index=todos.indexOf(todo)
   //todo.splice(index,i)
   const onDelete = (todo) => {
-    console.log('deleted', todo)
-    setTodos(todos.filter((item) => {
-      return item !== todo;
-    }));
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }
+    console.log("deleted", todo);
+    setTodos(
+      todos.filter((item) => {
+        return item !== todo;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const addTodo = (title, desc) => {
     let sno;
@@ -36,21 +39,21 @@ function App() {
     const myTodo = {
       sno: sno,
       title: title,
-      desc: desc
-    }
-    setTodos([...todos, myTodo])
-    console.log(myTodo)
-  }
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  };
 
   const [todos, setTodos] = useState(initTodo);
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
       <Router>
-        <Header title='MyTodos' searchBar={false} />
+        <Header title="MyTodos" searchBar={false} />
         <AddTodo addTodo={addTodo} />
         <Todos todos={todos} onDelete={onDelete} />
         <Footer />
@@ -85,4 +88,3 @@ export default App;
 // }
 
 // export default App;
-
